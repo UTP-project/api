c2bbe7fffd53f5f39fdfa4d8f568f955d07cfc86
ddowrs <whitezyg@gmail.com>
2020-04-17 15:12:04 +0800

    fix: remove 0 point preprocess

--- core/views.py
+++ core/views.py
@@ -16,17 +16,12 @@ def route(request):
     res = {"status": 0, "info": "error"}
     if request.method == "POST":
         post_data = json.loads(request.body)
-        # handle 0 point dist and dur
-        point_num = len(post_data.get("dist_matrix"))
-        dist_matrix = post_data.get("dist_matrix")
-        dur_matrix = post_data.get("dur_matrix")
-        dist_matrix = [[0] * (point_num + 1)] + [[0, *row] for row in dist_matrix]
-        dur_matrix = [[0] * (point_num + 1)] + [[0, *row] for row in dur_matrix]
         # create data
         data = {}
-        data["gene_num"] = point_num
-        data["dist_matrix"] = dist_matrix
-        data["time_matrix"] = dur_matrix
+        data["gene_num"] = len(post_data.get("dist_matrix") or [0]) - 1
+        data["locations"] = post_data.get("locations", [])
+        data["dist_matrix"] = post_data.get("dist_matrix", [])
+        data["time_matrix"] = post_data.get("dur_matrix", [])
         data["day_limit_time"] = post_data.get("day_limit_time", 8) * 60 * 60
         data["stay_time"] = post_data.get(
             "stay_time", [0, *[60 * 60] * data["gene_num"]]
